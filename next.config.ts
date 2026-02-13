e9d2598f5a06c422a577a52c2a9b8a2665a8a41b
PerfZero <gundenis1@gmail.com>
2026-02-13 14:40:35 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,38 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  output: "standalone",
   trailingSlash: true,
   images: {
     unoptimized: true,
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "header",
+            key: "accept",
+            value: ".*text/html.*",
+          },
+        ],
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-cache, no-store, max-age=0, must-revalidate",
+          },
+          {
+            key: "Pragma",
+            value: "no-cache",
+          },
+          {
+            key: "Expires",
+            value: "0",
+          },
+        ],
+      },
+    ];
   },
 };
 
